7267986b242cf05c3efb7576c7218032743f2f17
ladykakki <greta_calamari@hotmail.it>
2022-05-06 14:58:13 +0200

    icona spoty

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,33 @@
 <template>
-<div>
+<div class="header">
   
+  <img src="../assets/img/spoty.icon.png" alt="" class="">
+
 </div>
+
   
 </template>
 
 <script>
 export default {
+  name:"AppHeader",
+
+  
   
 }
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/style/vars.scss";
+
+
+.header{
+  background-color: $background;
+}
+
+img{
+  width: 5%;
+  
+}
 
 </style>
