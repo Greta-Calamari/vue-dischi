54cb36051494094d57f6a55e4f313f154beac843
ladykakki <greta_calamari@hotmail.it>
2022-05-06 15:04:35 +0200

    creata gia card e collegata a main

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,28 @@
   <div id="app">
     <header>
     <AppHeader/>
-
     </header>
+    <main>
+      <AppMain/>
+    </main>
+
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+
+
 
 export default {
   name: 'App',
   components: {
     AppHeader,
+    AppMain,
+    
+    
   }
 }
 </script>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="container">
+    <div class="col-6 col-md-4 col-lg-3 gy-3">
+        <AppCard/>
+    </div>
+
+
+
+</div>
+    
+</template>
+
+<script>
+// import axios from "axios";
+import AppCard from './AppCard.vue'
+
+export default {
+    name:"AppMain",
+    components:{
+        AppCard,
+    }
+    
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
